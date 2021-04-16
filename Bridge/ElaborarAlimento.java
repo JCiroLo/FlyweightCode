2c1cc2eb22b57d1e7de1ec3b11ca8511ca68ef55
jfbermudezbe <jfbermudezbe@unal.edu.co>
2021-04-15 23:41:42 -0500

    Bridge pattern

---
+++ Bridge/ElaborarAlimento.java
@@ -0,0 +1,24 @@
+package estructurales.bridge.bridge01;
+
+public abstract class ElaborarAlimento
+{
+    // Referencia al Implementador
+     IElaborar implementador;
+    String nombre;
+    // --------------------------
+    
+    public IElaborar getImplementador()
+    {
+        return this.implementador;
+    }
+
+    // --------------------------
+    public void setImplementador( IElaborar implementador )
+    {
+        this.implementador = implementador;
+    }
+
+    // --------------------------
+    // Método a implementar por las clases que hereden
+     public abstract void obtener();
+}
